331e1f470b288377b30e2d5c3c2ee87bba829318
alabgh01 <alabgh01@luther.edu>
2022-03-08 22:46:16 -0600

    exer6

--- exercise9/src/SalariedEmployee.cpp
+++ exercise9/src/SalariedEmployee.cpp
@@ -10,24 +10,33 @@
  */
 #include "SalariedEmployee.hpp"
 
-SalariedEmployee::SalariedEmployee(string name, string department,
-                                   double salary)
+SalariedEmployee::SalariedEmployee(string name, string department, double salary)
     : Employee(name, department), salary(salary) {
   /*TODO: Implement this method*/
+  this->setWeeklyPayment(getWeeklyPayment());
 }
 
 SalariedEmployee::SalariedEmployee(const SalariedEmployee &se)
     : Employee(se), salary(se.salary) {
   /*TODO: Implement this method*/
+    this->setDepartment(se.getDepartment());
+    this->setSalary(se.getSalary());
+    this->setWeeklyPayment(se.getWeeklyPayment());
 }
 
 double SalariedEmployee::getSalary() const { /*TODO: Implement this method*/
+  return this->salary;
 }
 
 void SalariedEmployee::setSalary(double salary) {
   /*TODO: Implement this method*/
+  this->salary = salary;
 }
 
 ostream &operator<<(ostream &os, const SalariedEmployee &se) {
   /*TODO: Implement this method*/
+  os << se.getName() << " from " << se.getDepartment() <<
+  " earned $" << se.getWeeklyPayment() << " this week with the annual contract of $" <<
+  fixed << setprecision(2) << se.getSalary();
+  return os;
 }
