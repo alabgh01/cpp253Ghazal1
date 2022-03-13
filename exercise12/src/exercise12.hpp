c46b14fc22ce6ee3db078166ff040de1303f593c
alabgh01 <alabgh01@luther.edu>
2022-03-13 15:56:20 -0500

    exercise 12

--- exercise12/src/exercise12.hpp
+++ exercise12/src/exercise12.hpp
@@ -16,23 +16,47 @@
 using namespace std;
 
 // Declare a function that uses an array.
+// takes an array and calculates the sum of its elements
+int TakeArray(int arr[], int size);
 
 // Declare a function that uses a vector.
+// takes a avector of words and makes a sentence
+string TakeVector(vector<string> vect);
 
 // Declare a function that uses a deque.
+// takes a deques, and does dq manipulation, add elements, remove and so on
+deque<int> TakeDeque(deque<int>, int size);
 
 // Declare a function that uses a list.
+// takes a list and checks which number is smalles, also allows user to predict it
+int TakeList(list<int> lst, int smallest);
 
 // Declare a function that uses a stack.
+// takes a stack and does stack manipulation and prints all stack elements
+void TakeStack(stack<int> st);
 
 // Declare a function that uses a queue.
+// takes a queue and does queue manipulation
+// asks user for input, add, remove and so on
+string TakeQueue(queue<string> q);
 
 // Declare a function that uses a priority_queue.
+// takes a queue and does priority queue manipulation
+// asks user for input, add, remove and so on
+int TakePQueue(priority_queue<int> pq);
 
 // Declare a function that uses a set.
+// takes a set and edits it by adding elements
+set<int> TakeSet(set<int> s);
 
 // Declare a function that uses a map.
+// takes words, rolls a dice and chooses words from
+// map randomly according to the dice result
+string TakeMap(int wNumber, map<string, string> wordsMap);
 
 // Declare a function that uses any other container.
+// takes a Multi set and edits it by adding elements, 
+// keepeing nuber repi\etiion
+int TakeMultiSet(multiset<string> s);
 
 #endif
--- exercise12/test/exercise12_test.cpp
+++ exercise12/test/exercise12_test.cpp
@@ -16,42 +16,197 @@
 #include "../src/exercise12.hpp"
 
 
-TEST_CASE ( "Test Array", "[test1]" ) {
+TEST_CASE ( "Test Array", "[TakeArray]" ) {
     /* Write an assertion here */
+    int arr[] = {1, 2, 3, 4, 5};
+    CHECK(TakeArray(arr, 5) == 15);
 }
 
-TEST_CASE ( "Test Vector", "[test2]" ) {
+TEST_CASE ( "Test Vector", "[TakeVector]" ) {
     /* Write an assertion here */
+    vector<string> vect{"I", "like", "c++"};
+    CHECK(TakeVector(vect) == "I like c++ ");
 }
 
-TEST_CASE ( "Test Deque", "[test3]" ) {
+TEST_CASE ( "Test Deque", "[TakeDeque]" ) {
     /* Write an assertion here */
+    deque<int> dq(1, 2);
+    deque<int> ndq = TakeDeque(dq, 2);
+    CHECK(ndq[0] == 2);
+    // CHECK(ndq[1] == 0);
 }
 
-TEST_CASE ( "Test List", "[test4]" ) {
+TEST_CASE ( "Test List", "[TakeList]" ) {
     /* Write an assertion here */
+    list<int> lst;
+    for (int i = 0; i < 5; ++i) {
+        lst.push_back(i);
+    }
+    CHECK(TakeList(lst, 2) == 1);
 }
 
-TEST_CASE ( "Test Stack", "[test5]" ) {
+TEST_CASE ( "Test Stack", "[TakeStack]" ) {
     /* Write an assertion here */
+    string expected = "stack not empty, we have: 3\nstack not empty, we have: 2\nstack not empty, we have: 1\n";
+    char fakeoutArr[1024];
+    stringstream fakeout(fakeoutArr);
+
+    streambuf *coutbuf = cout.rdbuf();
+    cout.rdbuf(fakeout.rdbuf());
+
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.pop();
+    TakeStack(st);
+    CHECK(fakeout.str() == expected);
+
+    cout.rdbuf(coutbuf);
 }
 
-TEST_CASE ( "Test Queue", "[test6]" ) {
+TEST_CASE ( "Test Queue", "[TakeQueue]" ) {
     /* Write an assertion here */
+    string expected = "front of queue is: str1 and back is: str4\nwhat do you want to do? (add, remove, or stop)\nstopped\n";
+    stringstream fakein("stop");
+    char fakeoutArr[1024];
+    stringstream fakeout(fakeoutArr);
+
+    streambuf *cinbuf = cin.rdbuf();
+    cin.rdbuf(fakein.rdbuf());
+
+    streambuf *coutbuf = cout.rdbuf();
+    cout.rdbuf(fakeout.rdbuf());
+
+    queue<string> q;
+    q.push("str1");
+    q.push("str2");
+    q.push("str3");
+    q.push("str4");
+    TakeQueue(q);
+    CHECK(fakeout.str() == expected);
+
+    cin.rdbuf(cinbuf);
+    cout.rdbuf(coutbuf);
+
+    // auto old_buffer = std::cout.rdbuf(nullptr);
+    // queue<string> q;
+    // q.push("str1");
+    // q.push("str2");
+    // q.push("str3");
+    // q.push("str4");
+    // CHECK(TakeQueue(q) == "str1");
 }
 
-TEST_CASE ( "Test Priority Queue", "[test7]" ) {
+TEST_CASE ( "Test Priority Queue", "[TakePQueue]" ) {
     /* Write an assertion here */
+
+    string expected = "prioritized element of queue is: 4\nwhat do you want to do? (add, remove, or stop)\nstopped\n";
+    stringstream fakein("stop");
+    char fakeoutArr[1024];
+    stringstream fakeout(fakeoutArr);
+
+    streambuf *cinbuf = cin.rdbuf();
+    cin.rdbuf(fakein.rdbuf());
+
+    streambuf *coutbuf = cout.rdbuf();
+    cout.rdbuf(fakeout.rdbuf());
+
+    priority_queue<int> pq;
+    pq.push(3);
+    pq.push(1);
+    pq.push(2);
+    pq.push(4);
+    TakePQueue(pq);
+    CHECK(fakeout.str() == expected);
+
+    cin.rdbuf(cinbuf);
+    cout.rdbuf(coutbuf);
+
+    // priority_queue<int> pq;
+    // pq.push(3);
+    // pq.push(1);
+    // pq.push(2);
+    // pq.push(4);
+    // CHECK(TakePQueue(pq) == 4);
 }
 
-TEST_CASE ( "Test Set", "[test8]" ) {
+TEST_CASE ( "Test Set", "[TakeSet]" ) {
     /* Write an assertion here */
+
+    string expected = "max size is: 461168601842738790\ndo you want to add to the set?\nstopped\n";
+    stringstream fakein("no");
+    char fakeoutArr[1024];
+    stringstream fakeout(fakeoutArr);
+
+    streambuf *cinbuf = cin.rdbuf();
+    cin.rdbuf(fakein.rdbuf());
+
+    streambuf *coutbuf = cout.rdbuf();
+    cout.rdbuf(fakeout.rdbuf());
+
+    set<int> s;
+    s.insert(3);
+    s.insert(1);
+    s.insert(2);
+    s.insert(4);
+    TakeSet(s);
+    CHECK(fakeout.str() == expected);
+
+    cin.rdbuf(cinbuf);
+    cout.rdbuf(coutbuf);
+    
+    // set<int> s;
+    // s.insert(3);
+    // s.insert(1);
+    // s.insert(2);
+    // s.insert(4);
+    // set<int> ss = TakeSet(s);
+    // CHECK(ss.size() == 4);
 }
 
-TEST_CASE ( "Test Map", "[test9]" ) {
+TEST_CASE ( "Test Map", "[TakeMap]" ) {
     /* Write an assertion here */
+    map<string, string> wordsMap;
+    wordsMap.insert({"1", "cpp"});
+    wordsMap.insert({"2", "253"});
+    wordsMap.insert({"3", "Luther"});
+    wordsMap.insert({"4", "College"});
+    wordsMap.insert({"5", "Iowa"});
+    wordsMap.insert({"6", "Decorah"});
+    CHECK(TakeMap(2, wordsMap) == "253 Iowa ");
 }
 
-TEST_CASE ( "Test Container", "[test10]" ) {
+TEST_CASE ( "Test Container", "[TakeMultiSet]" ) {
     /* Write an assertion here */
+
+    string expected = "the sentence is: It is nice today \ndo you want to add to the set?\nstopped\n";
+    stringstream fakein("no");
+    char fakeoutArr[1024];
+    stringstream fakeout(fakeoutArr);
+
+    streambuf *cinbuf = cin.rdbuf();
+    cin.rdbuf(fakein.rdbuf());
+
+    streambuf *coutbuf = cout.rdbuf();
+    cout.rdbuf(fakeout.rdbuf());
+
+    multiset<string> sm;
+    sm.insert("It");
+    sm.insert("is");
+    sm.insert("nice");
+    sm.insert("today");
+    TakeMultiSet(sm);
+    CHECK(fakeout.str() == expected);
+
+    cin.rdbuf(cinbuf);
+    cout.rdbuf(coutbuf);
+
+    // multiset<string> sm;
+    // sm.insert("It");
+    // sm.insert("is");
+    // sm.insert("nice");
+    // sm.insert("today");
+    // CHECK(TakeMultiSet(sm) == 4);
 }
